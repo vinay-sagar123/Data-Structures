d934d90e140031953df44f475bebb1f5ffe9a2cf
Vinay <vinaysagar4445@gmail.com>
2024-01-22 03:25:07 +0530

    This is the commit massage.This is the commit massage.

--- BFS.cpp
+++ BFS.cpp
@@ -1,3 +1,25 @@
 //
 // Created by 91952 on 22-01-2024.
 //
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int ver,edges; cin>>ver>>edges;
+    vector<int> adj[ver+1];
+    for(int i=1;i<=edges;i++)
+    {
+        int a,b; cin>>a>>b;
+        // If This is a directed Graph
+        adj[a].push_back(b);
+        adj[b].push_back(a);// This will not needed:
+    }
+    for(auto &it:adj)
+    {
+        for(auto &poi : it)
+        {
+            cout<<poi<<" ";
+        }
+        cout<<endl;
+    }
+}
--- Graph_representation.cpp
+++ Graph_representation.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int ver,edges; cin>>ver>>edges;
+    vector<int> adj[ver+1];
+    for(int i=1;i<=edges;i++)
+    {
+        int a,b; cin>>a>>b;
+        // If This is a directed Graph
+        adj[a].push_back(b);
+        adj[b].push_back(a);// This will not needed:
+    }
+    for(auto &it:adj)
+    {
+        for(auto &poi : it)
+        {
+            cout<<poi<<" ";
+        }
+        cout<<endl;
+    }
 }
